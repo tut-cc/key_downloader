967c0f83e457ce10deae44c05b99ae0740866b40
kimitoboku <emaxser@bonprosoft.com>
2015-03-19 23:18:19 +0900

    インデックスページを作成し，複数のダウンロードを行えるようにした

--- key_downloader.go
+++ key_downloader.go
@@ -12,7 +12,25 @@ type ZipFile struct {
 	Path string
 }
 
-func (z *ZipFile) Index(w http.ResponseWriter, r *http.Request) {
+type ZipList struct {
+	List []ZipFile
+}
+
+func (z *ZipList) Index(w http.ResponseWriter, r *http.Request) {
+	tmple, err := template.ParseFiles("index.html")
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = tmple.Execute(w, z)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (z *ZipFile) ItemIndex(w http.ResponseWriter, r *http.Request) {
 	tmple, err := template.ParseFiles("layout.html")
 
 	if err != nil {
@@ -59,8 +77,16 @@ func main() {
 	http.Handle("/layout/", http.StripPrefix("/layout/", http.FileServer(http.Dir("./layout"))))
 
 	zip := ZipFile{"test", "test.zip"}
-	http.HandleFunc("/", zip.Index)
-	http.HandleFunc(zip.DownloadPath(), zip.DownloadPage)
+	zipList := ZipList{
+		[]ZipFile{zip},
+	}
+
+	http.HandleFunc("/", zipList.Index)
+
+	for i := 0; i < len(zipList.List); i++ {
+		http.HandleFunc("/"+zipList.List[i].Name, zipList.List[i].ItemIndex)
+		http.HandleFunc(zipList.List[i].DownloadPath(), zipList.List[i].DownloadPage)
+	}
 
 	http.ListenAndServe(":8080", nil)
 }
